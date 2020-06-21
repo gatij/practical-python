817afdbe15dd8a3548f98fb29e88dc119c6db5e6
gatij <gatijjain@gmail.com>
2020-06-21 18:58:36 +0530

    section 1.1 to 1.3 completed

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
-# Exercise 1.5
+# Exercise 1.5 - 1.6
+bounce = 1
+height = 100
+while bounce <= 10:
+	height = (height*3)/5
+	print(bounce, round(height,4))
+	bounce = bounce + 1
+
+	
